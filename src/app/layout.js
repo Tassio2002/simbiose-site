ef7d697c24ae269a6284a348d03b3dce9e926583
Tassio2002git config --global user.email tassio.santos2002@gmail.com <tassio.santos2002@gmail.com>
2023-07-04 21:48:17 -0300

    :wrench: change project font family

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Jost } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const jost = Jost({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Simbiose Ventures',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={jost.className}>{children}</body>
     </html>
   )
 }
