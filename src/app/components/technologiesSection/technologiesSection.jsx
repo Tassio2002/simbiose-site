76314faf61f573f89a8065d37134df55b869972f
Erick Nascimento <erick.nascim3nto@gmail.com>
2023-07-07 17:39:49 -0300

    feat: technologies section with carousel

--- src/app/components/technologiesSection/technologiesSection.jsx
+++ src/app/components/technologiesSection/technologiesSection.jsx
@@ -1,10 +1,20 @@
+"use client";
 import "./styles/style.css";
 import TechnologiesCard from "@/app/components/technologiesCard/technologiesCard";
 import data from "@/app/data/home.json";
 import SectionName from "../global/SectionName/Enterprise";
+import Carousel from "nuka-carousel";
 
 const TechnologiesSection = () => {
   const TechSectionData = data.home.tecnologias;
+
+  const params = {
+    autoplay: false,
+    withoutControls: true,
+    autoplayInterval: 2000,
+    slidesToShow: 6,
+    wrapAround: false,
+  };
 
   return (
     <div className="technologies-section">
@@ -16,15 +26,17 @@ const TechnologiesSection = () => {
         <span>{TechSectionData.subtitle}</span>
       </div>
       <div className="technologies-cards">
-        {TechSectionData.cards.map((item, index) => {
-          return (
-            <TechnologiesCard
-              title={item.title}
-              icons={item.icons}
-              key={index}
-            />
-          );
-        })}
+        <Carousel {...params} className="languages-carousel">
+          {TechSectionData.cards.map((item, index) => {
+            return (
+              <TechnologiesCard
+                title={item.title}
+                icons={item.icons}
+                key={index}
+              />
+            );
+          })}
+        </Carousel>
       </div>
     </div>
   );
