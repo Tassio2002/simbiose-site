0302ba9b97992fc34ebb5d0da40f3193dda0f4a9
Vinicius Roque <hownatios@gmail.com>
2023-07-28 16:27:37 -0300

    feat: darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
