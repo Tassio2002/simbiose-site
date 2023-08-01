69df0b8867afaf0414cad6f27fb7ad4df8d7a5dc
Erick Nascimento <erick.nascim3nto@gmail.com>
2023-08-01 17:22:00 -0300

    fix: add class in textarea

--- src/app/components/home/ContactSection/ContactForm.jsx
+++ src/app/components/home/ContactSection/ContactForm.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./styles/style.css";
 import data from "@/app/data/home.json";
 import dataButton from "@/app/data/global-components.json";
 import Image from "next/image";
 
 const ContactForm = () => {
-
   const buttonData = dataButton.globalButton;
 
   const formData = data.home.contato.form.background;
@@ -25,12 +24,11 @@ const ContactForm = () => {
     event.preventDefault();
 
     const values = {
-      'name': event.target[0].value,
-      'email': event.target[1].value,
-      'phone' : event.target[2].value,
-      'message': event.target[3].value
-
-    }
+      name: event.target[0].value,
+      email: event.target[1].value,
+      phone: event.target[2].value,
+      message: event.target[3].value,
+    };
 
     try {
       await fetch("/api/contact", {
@@ -40,40 +38,40 @@ const ContactForm = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(values),
-      }).then(response =>
-        response.status === 200 ? sendedFlow() : ''
-      )
+      }).then((response) => (response.status === 200 ? sendedFlow() : ""));
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  if(emailSent){
+  if (emailSent) {
     return (
       <div className="form-container-post-shipment">
         <div style={style.background}>
-          <h2 className={'postShipmentTitle'}>Agradecemos o contato!</h2>
-          <h3 className={'postShipmentText'}>Estaremos retornando o mais breve possível.</h3>
+          <h2 className={"postShipmentTitle"}>Agradecemos o contato!</h2>
+          <h3 className={"postShipmentText"}>
+            Estaremos retornando o mais breve possível.
+          </h3>
         </div>
       </div>
     );
-  }
-  else {
+  } else {
     return (
       <div className="form-container">
         <form style={style.background} onSubmit={handleSubmit}>
           <h2>Tem alguma dúvida?</h2>
-          <input type="text" placeholder="* Nome"/>
-          <input type="email" placeholder="* E-mail"/>
-          <input type="tel" placeholder="Telefone"/>
+          <input type="text" placeholder="* Nome" />
+          <input type="email" placeholder="* E-mail" />
+          <input type="tel" placeholder="Telefone" />
           <textarea
+            className="inputMsg"
             cols="30"
             rows="6"
             placeholder="* Conte como podemos ajudá-lo."
           ></textarea>
-          <button type={'submit'} className="form-btn button-container">
+          <button type={"submit"} className="form-btn button-container">
             <p>Enviar</p>
-            <Image src={buttonData.icon} width={17} height={8} alt={"Icon"}/>
+            <Image src={buttonData.icon} width={17} height={8} alt={"Icon"} />
           </button>
         </form>
       </div>
