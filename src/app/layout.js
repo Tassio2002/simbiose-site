f978283edf0de293eaf26c3224d98783ea240e9a
Diego <diego.o.guimaraes29@gmail.com>
2024-10-02 16:18:47 -0300

    Fix: gtm tag style

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,17 +22,16 @@ export default function RootLayout({ children }) {
           content="upgrade-insecure-requests"
         />
         <meta name="description" content={metadata.description} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KW3DCQDZ')`,
-          }}
-        />{" "}
       </Head>
       <body className={jost.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KW3DCQDZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <MobileObserverContextProvider>
           <SetServicePageIndexContextProvider>
             <ServiceContextProvider>
@@ -66,14 +65,15 @@ export default function RootLayout({ children }) {
                   gtag('config', 'AW-11182275938');
                 `}
               </Script>
-              <noscript>
-                <iframe
-                  src="https://www.googletagmanager.com/ns.html?id=GTM-KW3DCQDZ"
-                  height="0"
-                  width="0"
-                  style="display:none;visibility:hidden"
-                ></iframe>
-              </noscript>
+              <Script id="form" strategy="afterInteractive">
+                {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-KW3DCQDZ');
+                `}
+              </Script>
             </ServiceContextProvider>
           </SetServicePageIndexContextProvider>
         </MobileObserverContextProvider>
